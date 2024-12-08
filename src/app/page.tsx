3a6ddac9a05cacb95b5966b10337c74cd9fbe775
yousrakhan23 <ammarahkhan2016@gmail.com>
2024-12-08 06:01:35 +0500

    nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import React from 'react';
 import Header from '@/components/Header';
+import Navbar from '@/components/Navbar';
 const HomePage = () => {
   return (
     <div>
       <Header />
+      <Navbar />
     </div>
   )
 }
