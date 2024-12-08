94eb1e8eabf700ab8a5c86e5e90de48bfd8250b6
yousrakhan23 <ammarahkhan2016@gmail.com>
2024-12-08 10:17:12 +0500

    footer done with responsivenesss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import Header from '@/components/Header';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -26,9 +28,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+      
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
